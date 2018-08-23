5a845044360caa80a44c2837784cec1300dae08c
Max <vloxomire@gmail.com>
2018-08-23 00:44:48 -0300

    Models

--- app/src/main/java/com/max/sqlitecalculadoramax/Models/Operacion.java
+++ app/src/main/java/com/max/sqlitecalculadoramax/Models/Operacion.java
@@ -1,4 +1,27 @@
 package com.max.sqlitecalculadoramax.Models;
 
 public class Operacion {
+    private Integer id;
+    private String operacion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public Operacion(Integer id, String operacion) {
+        this.id = id;
+        this.operacion = operacion;
+    }
 }
