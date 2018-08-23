19a806ceb5e7e72ba29c278813a976be4474f1fb
Max <vloxomire@gmail.com>
2018-08-23 00:36:16 -0300

    DaoCalculadora

---
+++ app/src/main/java/com/max/sqlitecalculadoramax/Dao/SqliteCalculadora.java
@@ -0,0 +1,41 @@
+package com.max.sqlitecalculadoramax.Dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqliteCalculadora extends SQLiteOpenHelper{
+    private static final String DBNAME="calculadora";
+    private static final Integer DBVERSION=1;
+    private Context context;
+    private SQLiteDatabase conexion;
+
+    public SqliteCalculadora(Context context) {
+        super(context, DBNAME, null, DBVERSION);
+        this.context = context;
+    }
+        @Override
+        public void onCreate (SQLiteDatabase db){
+
+        }
+
+        @Override
+        public void onUpgrade (SQLiteDatabase db,int oldVersion, int newVersion){
+        String query="CREATE TABLE `calculadora` ( `Id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `operacion` TEXT NOT NULL )";
+        db.execSQL(query);
+        }
+        private void conectar(){
+        conexion=getWritableDatabase();
+        }
+        private void desconectar(){
+        conexion.close();
+        }
+        public void guardarOperaciones(){
+        this.conectar();
+        ContentValues row= new ContentValues();
+        row.put("operacion", oper);
+        conexion.insert("operacion",null,row);
+        this.desconectar();
+        }
+    }
---
+++ app/src/main/java/com/max/sqlitecalculadoramax/Models/Operacion.java
@@ -0,0 +1,4 @@
+package com.max.sqlitecalculadoramax.Models;
+
+public class Operacion {
+}
