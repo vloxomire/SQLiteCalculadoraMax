d8967981d9334d2bf2a42ac7baa242e8abcd63d9
max <vloxomire@gmail.com>
2018-08-23 21:13:34 -0300

    ListenerRestaSqlite

--- app/src/main/java/com/max/sqlitecalculadoramax/Listener/ListenerResta.java
+++ app/src/main/java/com/max/sqlitecalculadoramax/Listener/ListenerResta.java
@@ -2,10 +2,13 @@ package com.max.sqlitecalculadoramax.Listener;
 
 import android.view.View;
 
+import com.max.sqlitecalculadoramax.Dao.SqliteCalculadora;
 import com.max.sqlitecalculadoramax.Interfaz.SqlitecalculadoramaxActivity;
+import com.max.sqlitecalculadoramax.Models.Operacion;
 
 public class ListenerResta implements View.OnClickListener{
     SqlitecalculadoramaxActivity context;
+    SqliteCalculadora sqliteCalculadora;
 
     public ListenerResta(SqlitecalculadoramaxActivity context) {
         this.context = context;
@@ -13,9 +16,12 @@ public class ListenerResta implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
-    Integer nro1=Integer.parseInt(context.getEt1().getText().toString());
-    Integer nro2=Integer.parseInt(context.getEt2().getText().toString());
-    Integer resResta=nro1 - nro2;
-    context.getTv1().setText(String.valueOf(resResta));
+        sqliteCalculadora = new SqliteCalculadora(context);
+        Integer nro1=Integer.parseInt(context.getEt1().getText().toString());
+        Integer nro2=Integer.parseInt(context.getEt2().getText().toString());
+        Integer resResta=nro1 - nro2;
+        context.getTv1().setText(String.valueOf(resResta));
+        Operacion operacion= new Operacion(null,(nro1 +" - "+ nro2 +"="+resResta));
+        sqliteCalculadora.guardarOperacionSinSQLite(operacion);
     }
 }
